2769cbf1c9a55e1497f9c0db02079bc0a7928b31
Sheen An Goh <sheen.an.goh@gmail.com>
2020-08-31 17:06:42 +0800

    First commit

--- lib/workshop-typescript-stack.ts
+++ lib/workshop-typescript-stack.ts
@@ -2,17 +2,47 @@ import * as sns from '@aws-cdk/aws-sns';
 import * as subs from '@aws-cdk/aws-sns-subscriptions';
 import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import { Bucket, BlockPublicAccess } from '@aws-cdk/aws-s3';
+import {
+  Instance,
+  InstanceType,
+  InstanceClass,
+  InstanceSize,
+  AmazonLinuxImage,
+  Vpc,
+} from '@aws-cdk/aws-ec2';
 
 export class WorkshopTypescriptStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    /**
     const queue = new sqs.Queue(this, 'WorkshopTypescriptQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+      visibilityTimeout: cdk.Duration.seconds(300),
     });
 
     const topic = new sns.Topic(this, 'WorkshopTypescriptTopic');
 
+    const bucket = new Bucket(this, 'cdkbucket', {
+      versioned: true,
+      blockPublicAccess: new BlockPublicAccess({
+        blockPublicAcls: true,
+        blockPublicPolicy: true,
+        ignorePublicAcls: true,
+        restrictPublicBuckets: true,
+      }),
+    });
+
+    const vpc = new Vpc(this, 'VPC');
+
+    const ec2 = new Instance(this, 'cdkec2instance', {
+      vpc,
+      instanceType: InstanceType.of(InstanceClass.T2, InstanceSize.MICRO),
+      machineImage: new AmazonLinuxImage(),
+    });
+
     topic.addSubscription(new subs.SqsSubscription(queue));
+
+     */
   }
 }
